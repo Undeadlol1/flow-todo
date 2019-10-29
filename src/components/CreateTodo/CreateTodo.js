4dcfb035236825273aacd8f71ae5316f261e7137
Mihail Paley <undeadlol1@mail.ru>
2019-10-29 23:55:05 +0500

    add eslint

--- src/components/CreateTodo/CreateTodo.js
+++ src/components/CreateTodo/CreateTodo.js
@@ -1,38 +1,40 @@
 import React from 'react';
 import * as Yup from 'yup';
-import get from 'lodash/get'
-import useForm from 'react-hook-form'
-import isUndefined from 'lodash/isUndefined'
+import get from 'lodash/get';
+import useForm from 'react-hook-form';
+import isUndefined from 'lodash/isUndefined';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 
 const validationSchema = Yup.object({
-    todoName: Yup.string()
-        .min(3, 'Не менее 3 символов')
-        .required('Обязательно')
-})
+  todoName: Yup.string()
+    .min(3, 'Не менее 3 символов')
+    .required('Обязательно'),
+});
 
 export default function CreateTodo(props) {
-    const { register, handleSubmit, formState, errors, reset } = useForm({ validationSchema })
-    const error = props.error || get(errors, 'todoName.message')
-    const isSubmitDisabled = isUndefined(props.isValid) ?
-        (error || formState.isSubmitting) :
-        true
-    function onSubmit(values) {
-        console.log('values: ', values);
-        reset({})
-    }
-    return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <TextField
-                label="Создать задачу"
-                name="todoName"
-                error={Boolean(error)}
-                inputRef={register}
-                helperText={error}
-            />
-            <br />
-            <Button disabled={isSubmitDisabled} type="submit">Сохранить</Button>
-        </form>
-    )
+  const {
+    register, handleSubmit, formState, errors, reset,
+  } = useForm({ validationSchema });
+  const error = props.error || get(errors, 'todoName.message');
+  const isSubmitDisabled = isUndefined(props.isValid)
+    ? (error || formState.isSubmitting)
+    : true;
+  function onSubmit(values) {
+    console.log('values: ', values);
+    reset({});
+  }
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <TextField
+        label="Создать задачу"
+        name="todoName"
+        error={Boolean(error)}
+        inputRef={register}
+        helperText={error}
+      />
+      <br />
+      <Button disabled={isSubmitDisabled} type="submit">Сохранить</Button>
+    </form>
+  );
 }

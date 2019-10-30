8917aab678ed134ebca6ea00df4f9c04fb1b31f5
Mihail Paley <undeadlol1@mail.ru>
2019-10-30 15:36:11 +0500

    Change App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import CreateTask from './components/tasks/CreateTask/CreateTask';
+import NavBar from './components/ui/NavBar/NavBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <CreateTask />
     </div>
   );
 }

292f1dc193bb2f8f81eeaaa17366de298f021193
Mihail Paley <undeadlol1@mail.ru>
2019-10-30 21:19:02 +0500

    add grid to HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
 import { CreateTaskContainer } from '../components/tasks/CreateTask/CreateTask';
 import { TasksListContainer } from '../components/tasks/TasksList/TasksList';
 
 export default function HomePage() {
   return (
     <>
-      <CreateTaskContainer />
-      <TasksListContainer />
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="center"
+      >
+        <Grid item xs={12} align="center">
+          <CreateTaskContainer />
+        </Grid>
+        <Grid item xs={12} align="center">
+          <TasksListContainer />
+        </Grid>
+      </Grid>
     </>
   );
 }

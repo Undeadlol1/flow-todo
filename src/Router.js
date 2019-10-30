06de5017648454b7a0a3a193938886ebea7866df
Mihail Paley <undeadlol1@mail.ru>
2019-10-30 23:59:40 +0500

    add simple TaskPage

--- src/Router.js
+++ src/Router.js
@@ -7,12 +7,16 @@ import {
 import NavBar from './components/ui/NavBar/NavBar';
 import SignInPage from './pages/SignInPage';
 import HomePage from './pages/HomePage';
+import TaskPage from './pages/TaskPage';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <NavBar />
       <Switch>
+        <Route path="/task/:taskId">
+          <TaskPage />
+        </Route>
         <Route path="/signIn">
           <SignInPage />
         </Route>

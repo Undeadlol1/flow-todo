9648579e9d319fa7aa71e2fea70b11b79df18fe9
Mihail Paley <undeadlol1@mail.ru>
2019-10-31 00:37:49 +0500

    add Grid to TaskPage

--- src/pages/TaskPage.js
+++ src/pages/TaskPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Grid from '@material-ui/core/Grid';
 import { firestore } from 'firebase/app';
 import { useParams } from 'react-router-dom';
 import { useDocumentData } from 'react-firebase-hooks/firestore';
@@ -8,9 +9,9 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 export function TaskPage(props) {
   if (props.loading) return <CircularProgress />;
   return (
-    <div>
+    <Grid container>
       <h2>{props.task.name}</h2>
-    </div>
+    </Grid>
   );
 }
 

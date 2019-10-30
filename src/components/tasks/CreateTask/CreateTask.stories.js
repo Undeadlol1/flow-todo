9fc5e0db5bdcd2d4af991c202f5b6c2ceafa427f
Mihail Paley <undeadlol1@mail.ru>
2019-10-30 14:27:31 +0500

    add PropTypes to CreateTodo

--- src/components/tasks/CreateTask/CreateTask.stories.js
+++ src/components/tasks/CreateTask/CreateTask.stories.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import CreateTask from './CreateTask'
+import CreateTask from './CreateTask';
 
 export default {
-    component: CreateTask,
-    title: 'CreateTask',
+  component: CreateTask,
+  title: 'CreateTask',
 };
 
 export const normal = () => <CreateTask />;
 
-export const invalid = () => <CreateTask isValid={false} />
+export const invalid = () => <CreateTask isValid={false} />;
 
-export const withError = () => <CreateTask error="Error occured" />
+export const withError = () => <CreateTask error="Error occured" />;
 
-withError.title = "With error"
+withError.title = 'With error';

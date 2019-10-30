1f5880581b69d1e600043007598573b23e69c017
Mihail Paley <undeadlol1@mail.ru>
2019-10-31 00:34:10 +0500

    HomePage refactoring

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,20 +5,18 @@ import { TasksListContainer } from '../components/tasks/TasksList/TasksList';
 
 export default function HomePage() {
   return (
-    <>
-      <Grid
-        container
-        direction="row"
-        justify="center"
-        alignItems="center"
-      >
-        <Grid item xs={12} align="center">
-          <CreateTaskContainer />
-        </Grid>
-        <Grid item xs={12} sm={8} md={4} lg={4} align="center">
-          <TasksListContainer />
-        </Grid>
+    <Grid
+      container
+      direction="row"
+      justify="center"
+      alignItems="center"
+    >
+      <Grid item xs={12} align="center">
+        <CreateTaskContainer />
       </Grid>
-    </>
+      <Grid item xs={12} sm={8} md={4} lg={4} align="center">
+        <TasksListContainer />
+      </Grid>
+    </Grid>
   );
 }

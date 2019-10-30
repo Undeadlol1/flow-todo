4b4ef5d97fb31af114ccfaff34e61499fe5738ea
Mihail Paley <undeadlol1@mail.ru>
2019-10-30 13:17:19 +0500

    fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,6 @@ module.exports = {
   ],
   rules: {
     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-    "react/destructuring-assignment": [<enabled>, "never", {"ignoreClassFields": <boolean> }]
+    "react/destructuring-assignment": [2, "never"]
   },
 };

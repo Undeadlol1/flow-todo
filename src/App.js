7c3535ee5ed4787c6991f1398482fa4179c8aa18
Mihail Paley <undeadlol1@mail.ru>
2019-10-30 15:48:37 +0500

    add firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from 'firebase/app';
+import 'firebase/analytics';
+import 'firebase/auth';
+import 'firebase/firestore';
+
 import React from 'react';
 import NavBar from './components/ui/NavBar/NavBar';
 import CreateTask from './components/tasks/CreateTask/CreateTask';
 import './App.css';
+
+// Initialize Firebase
+firebase.initializeApp({
+  apiKey: 'AIzaSyAmCyhaB-8xjMH5yi9PoitoAyD-KeFnNtA',
+  authDomain: 'flow-todo-5824b.firebaseapp.com',
+  databaseURL: 'https://flow-todo-5824b.firebaseio.com',
+  projectId: 'flow-todo-5824b',
+  storageBucket: 'flow-todo-5824b.appspot.com',
+  messagingSenderId: '772125171665',
+  appId: '1:772125171665:web:3fffadc4031335de290af0',
+  measurementId: 'G-DLFD2VSSK1',
+});
 
 function App() {
   return (

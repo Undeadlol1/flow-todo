8c2c8efd798f78b863a94139ac2b23bc9602cd5f
Mihail Paley <undeadlol1@mail.ru>
2019-10-31 00:23:08 +0500

    fetch task in TaskPage

--- src/pages/TaskPage.js
+++ src/pages/TaskPage.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { firestore } from 'firebase/app';
+import { useParams } from 'react-router-dom';
+import { useDocumentData } from 'react-firebase-hooks/firestore';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
-export default function TaskPage() {
+export function TaskPage(props) {
+  if (props.loading) return <CircularProgress />;
   return (
     <div>
-      <h1>HI</h1>
+      <h2>{props.task.name}</h2>
     </div>
   );
 }
+
+TaskPage.propTypes = {
+  task: PropTypes.object,
+  loading: PropTypes.bool.isRequired,
+  taskId: PropTypes.string.isRequired,
+};
+
+export default (props) => {
+  const { taskId } = useParams();
+  const [task, loading] = useDocumentData(
+    firestore().collection('tasks').doc(taskId),
+  );
+  return (
+    <TaskPage {...{
+      task, loading, taskId, ...props,
+    }}
+    />
+  );
+};

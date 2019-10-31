3f083d81add6d506f388be1c578b822222fa186a
Mihail Paley <undeadlol1@mail.ru>
2019-10-31 14:08:02 +0500

    add TaskActions to TaskPage

--- src/pages/TaskPage.js
+++ src/pages/TaskPage.js
@@ -2,21 +2,67 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
 import { firestore } from 'firebase/app';
-import { useParams } from 'react-router-dom';
+import {
+  useParams,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useLocation,
+} from 'react-router-dom';
 import { useDocumentData } from 'react-firebase-hooks/firestore';
+import Typography from '@material-ui/core/Typography';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Button from '@material-ui/core/Button';
+
+
+const TaskActions = (props) => {
+  const { pathname } = useLocation();
+  return (
+    <Grid container direction="column">
+      <Grid item xs align="center">
+        <Button component={Link} to={`${pathname}/hard`}>Мне трудно</Button>
+      </Grid>
+      <Grid item xs align="center">
+        <Button>Сделал шаг вперед</Button>
+      </Grid>
+      <Grid item xs align="center">
+        <Button>Сильно продвинулся</Button>
+      </Grid>
+    </Grid>
+  );
+};
 
 export function TaskPage(props) {
-  if (props.loading) return <CircularProgress />;
+  if (props.loading) {
+    return (
+      <Grid item xs align="center">
+        <CircularProgress />
+      </Grid>
+    );
+  }
   return (
-    <Grid container>
-      <h2>{props.task.name}</h2>
+    <Grid container direction="column">
+      <Grid item xs align="center">
+        <Link to={`/task/${props.taskId}`}>
+          <Typography variant="h2">{props.task.name}</Typography>
+        </Link>
+      </Grid>
+      <Switch>
+        <Route path={`${props.path}/hard`}>
+          <h1>This is hard indeed</h1>
+        </Route>
+        <Route path={props.path}>
+          <TaskActions {...props} />
+        </Route>
+      </Switch>
     </Grid>
   );
 }
 
 TaskPage.propTypes = {
   task: PropTypes.object,
+  path: PropTypes.string.isRequired,
   loading: PropTypes.bool.isRequired,
   taskId: PropTypes.string.isRequired,
 };
@@ -26,8 +72,9 @@ export default (props) => {
   const [task, loading] = useDocumentData(
     firestore().collection('tasks').doc(taskId),
   );
+  const { path } = useRouteMatch();
   const mergedProps = {
-    task, loading, taskId, ...props,
+    task, loading, taskId, path, ...props,
   };
   return (
     <TaskPage {...mergedProps} />

93a1a6f7bf4a26b0bc3eba5ee8ba0b9d65d3dcfa
Mihail Paley <undeadlol1@mail.ru>
2019-11-01 12:44:49 +0500

    allow object proptypes in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,5 +25,6 @@ module.exports = {
     "react/destructuring-assignment": [0, "never"],
     "react/jsx-props-no-spreading": [0, "never"],
     "import/no-named-as-default": [0, "never"],
+    "react/forbid-prop-types": [0, "never"],
   },
 };

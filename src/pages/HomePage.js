5d4dce95bd8e01d351e2af25ad8a420a1b4f8628
Mihail Paley <undeadlol1@mail.ru>
2019-11-01 21:57:49 +0500

    Improve grid spacing of HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,11 +7,12 @@ export default function HomePage() {
   return (
     <Grid
       container
-      direction="row"
+      spacing={4}
       justify="center"
+      direction="column"
       alignItems="center"
     >
-      <Grid item xs={12} align="center">
+      <Grid item xs={12} sm={12} align="center">
         <CreateTask />
       </Grid>
       <Grid item xs={12} sm={8} md={4} lg={4} align="center">

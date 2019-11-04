4d219e7778e2f6f176a798e89c381448fbf52b07
Mihail Paley <undeadlol1@mail.ru>
2019-11-04 13:41:21 +0500

    change servcie worker commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
+// Make app work offline and load faster.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register();

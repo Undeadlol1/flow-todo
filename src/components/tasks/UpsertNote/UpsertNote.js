3ff4ba21a1ab75fd3eb1b0876f99ca991702d8f4
Mihail Paley <undeadlol1@mail.ru>
2019-11-05 14:57:54 +0500

    (WIP) add UpsertNote component template

---
+++ src/components/tasks/UpsertNote/UpsertNote.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import useForm from 'react-hook-form';
+
+const UpsertNote = props => {
+    const { values } = useForm();
+    return (
+      <div />
+    );
+};
+
+UpsertNote.propTypes = {
+
+};
+
+export default UpsertNote;

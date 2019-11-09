1b19bbc416d2ced4e503e003edfbb74117f8cf44
Mihail Paley <undeadlol1@mail.ru>
2019-11-09 17:26:40 +0500

    add usefull properties to subtask on creation

--- src/components/tasks/CreateSubtask/CreateSubtask.js
+++ src/components/tasks/CreateSubtask/CreateSubtask.js
@@ -8,29 +8,34 @@ import { useTranslation } from 'react-i18next';
 import * as Yup from 'yup';
 import get from 'lodash/get';
 import Paper from '@material-ui/core/Paper';
+import nanoid from 'nanoid';
 
 const CreateSubtask = props => {
   const [t] = useTranslation();
   const {
     register, handleSubmit, errors, reset, setError,
-  } = useForm(
-    {
-      validationSchema: Yup.object({
-        name: Yup.string()
-          .min(3, t('validation.atleast3Symbols'))
-          .required(t('validation.required')),
-      }),
-    },
-  );
+  } = useForm({
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .min(3, t('validation.atleast3Symbols'))
+        .required(t('validation.required')),
+    }),
+  });
   const error = get(errors, 'name.message');
 
   function createSubtask(values) {
+    const subtask = {
+      id: nanoid(),
+      isDone: false,
+      createdAt: Date.now(),
+      name: values.name.trim(),
+    };
     //  TODO i need to put it into a reusable function
     return firestore()
       .collection('tasks')
       .doc(props.taskId)
       .update({
-        subtasks: firestore.FieldValue.arrayUnion(values),
+        subtasks: firestore.FieldValue.arrayUnion(subtask),
       })
       .then(() => reset({}))
       .catch(e => setError(e && e.message));

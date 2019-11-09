5e5fea7ce03bc109e9623556ce301762f4243c83
Mihail Paley <undeadlol1@mail.ru>
2019-11-09 20:52:58 +0500

    add "updateSubtask" function

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,7 @@
 import nanoid from 'nanoid';
 import { firestore } from 'firebase/app';
+import updateWith from 'lodash/updateWith'
+import extend from 'lodash/extend'
 
 export function createSubtask(taskId: string, values: {
         name: string,
@@ -10,12 +12,41 @@ export function createSubtask(taskId: string, values: {
             subtasks: firestore.FieldValue.arrayUnion({
                 id: nanoid(),
                 isDone: false,
+                parentId: taskId,
                 createdAt: Date.now(),
                 name: values.name.trim(),
             }),
         })
 }
 
+interface SubtaskType {
+    id: string,
+    isDone: boolean,
+    parentId: string,
+    createdAt: string,
+    name: string,
+}
+
+export function updateSubtask(subtask: SubtaskType, values: {
+        name: string,
+        doneAt: string,
+        isDone: boolean,
+    },) : Promise<void | Error> {
+    return firestore()
+        .doc('tasks/'+ subtask.parentId)
+        .get()
+        .then((task: any) : Promise <void | Error>  => {
+            const {subtasks} = task.docs()
+            const newSubtask: any = extend(subtask, values)
+            return task.update({
+                subtasks: <Array<SubtaskType>>updateWith(
+                    subtasks,
+                    '[' + subtasks.findIndex(subtask) + ']',
+                    newSubtask
+            )
+        })
+    })
+}
 
 export function deleteSubtask(taskId: string, subtask: {
     id: string,
@@ -26,5 +57,3 @@ export function deleteSubtask(taskId: string, subtask: {
             subtasks: firestore.FieldValue.arrayRemove(subtask),
         })
 }
-
-export default '';

df2d7e94e4c27cc59d875172f7dbb3f530ca1b76
Mihail Paley <undeadlol1@mail.ru>
2019-11-10 18:23:24 +0500

    remove unused imports

--- src/components/tasks/HardChoices.tsx
+++ src/components/tasks/HardChoices.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import { useTranslation } from 'react-i18next';
-import Slide from '@material-ui/core/Slide';
 import CreateSubtask from './CreateSubtask/CreateSubtask';
 import SubtasksList from './SubtasksList';
 import { makeStyles } from '@material-ui/core/styles';

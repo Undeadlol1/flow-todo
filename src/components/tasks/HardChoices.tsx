b99ad96804796f69dc16bb6b9c01a87ad74f5158
Mihail Paley <undeadlol1@mail.ru>
2019-11-10 23:02:01 +0500

    add CreateTask to TaskChoices

--- src/components/tasks/HardChoices.tsx
+++ src/components/tasks/HardChoices.tsx
@@ -1,33 +1,25 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-import { useTranslation } from 'react-i18next';
 import CreateSubtask from './CreateSubtask/CreateSubtask';
 import SubtasksList from './SubtasksList';
 import { makeStyles } from '@material-ui/core/styles';
+import CreateTask from './CreateTask/UpsertTask';
 
 const useStyles = makeStyles(theme => ({
   form: {
-    marginBottom: theme.spacing(1),
-  },
-  button: {
-    display: 'block',
-    margin: '0 auto',
+    marginBottom: theme.spacing(1)
   },
 }));
 
 const HardChoices = (props: {
   taskId: string;
-  task: { subtasks: any };
+  task: { name: string, subtasks: any };
 }) => {
-  const [t] = useTranslation();
   const classes = useStyles();
   return (
     <>
       <Grid item xs={12} sm={8} md={6} lg={5}>
-        <Button className={classes.button} disabled>
-          {t('Rework task')}
-        </Button>
+        <CreateTask taskId={props.taskId} defaultValue={props.task.name} />
       </Grid>
       <Grid item xs={12} sm={12} md={12} lg={12} />
       <Grid item xs={12} sm={8} md={6} lg={5}>

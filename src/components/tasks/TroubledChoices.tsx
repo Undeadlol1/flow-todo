517370276bb9f9068541587aebbadfce6dab799c
Mihail Paley <undeadlol1@mail.ru>
2019-11-11 15:50:02 +0500

    (TroubledChoices) improve button styles

--- src/components/tasks/TroubledChoices.tsx
+++ src/components/tasks/TroubledChoices.tsx
@@ -6,14 +6,27 @@ import addHours from 'date-fns/addHours';
 import Slide from '@material-ui/core/Slide';
 import { Link, useLocation } from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
 
 const TroublesChoices = ({
   updateTask,
 }: {
   updateTask: Function;
 }) => {
+  const classes = useStyles();
   const [t] = useTranslation();
   const { pathname } = useLocation();
+  const commonButtonProps = {
+    color: 'primary',
+    variant: 'contained',
+    className: classes.button,
+  };
   function postPone() {
     updateTask(
       { dueAt: addHours(new Date(), 16).getTime() },
@@ -25,15 +38,35 @@ const TroublesChoices = ({
     <Slide in direction="left">
       <div style={{ textAlign: 'center' }}>
         <Grid item xs={12}>
-          <Button component={Link} to={`${pathname}/isHard`}>
+          <Button
+            component={Link}
+            to={`${pathname}/isHard`}
+            color="primary"
+            variant="contained"
+            className={classes.button}
+          >
             {t('hard')}
           </Button>
         </Grid>
         <Grid item xs={12}>
-          <Button disabled>{t('dont want to')}</Button>
+          <Button
+            className={classes.button}
+            variant="contained"
+            color="primary"
+            disabled
+          >
+            {t('dont want to')}
+          </Button>
         </Grid>
         <Grid item xs={12}>
-          <Button onClick={postPone}>{t('cant right now')}</Button>
+          <Button
+            color="primary"
+            variant="contained"
+            className={classes.button}
+            onClick={postPone}
+          >
+            {t('cant right now')}
+          </Button>
         </Grid>
       </div>
     </Slide>

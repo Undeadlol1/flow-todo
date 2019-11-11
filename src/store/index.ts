b85c502bf6a92b90ead5dfb295fd836a47cb54d8
Mihail Paley <undeadlol1@mail.ru>
2019-11-11 11:21:00 +0500

    fix "upsertTask" creating document with missing properties

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,89 +3,106 @@ import { firestore } from 'firebase/app';
 import subtractDays from 'date-fns/subDays';
 
 interface ITask {
-    name: string,
-    isDone: boolean,
-    doneAt?: string,
-    dueAt: string,
-    userId: string,
-    subtasks?: any[],
+  name: string;
+  isDone: boolean;
+  doneAt?: string;
+  dueAt: string;
+  userId: string;
+  subtasks?: any[];
 }
 
-export function upsertTask(values: { name: string, userId?: string }, taskId?: string): Promise<void | Error> {
-    if (!taskId && !values.userId) return new Promise((resolve, reject) => reject('You forgot to add userId'))
-    return firestore()
-        .collection('tasks')
-        .doc(taskId || nanoid())
-        .set(
-            taskId
-                ? values
-                : {
-                    isDone: false,
-                    dueAt: subtractDays(new Date(), 1).getTime(),
-                }
-        )
-}
-
-// TODO: is this function ever used? Remove it?
-export function createTask(values: { name: string, userId: string }): Promise<firestore.DocumentReference> {
-    return firestore()
-        .collection('tasks')
-        .add({
+export function upsertTask(
+  values: { name: string; userId?: string },
+  taskId?: string,
+): Promise<void | Error> {
+  if (!taskId && !values.userId)
+    return new Promise((resolve, reject) =>
+      reject('You forgot to add userId'),
+    );
+  return firestore()
+    .collection('tasks')
+    .doc(taskId || nanoid())
+    .set(
+      taskId
+        ? values
+        : {
             ...values,
             isDone: false,
             dueAt: subtractDays(new Date(), 1).getTime(),
-        })
+          },
+    );
 }
 
-export function createSubtask(taskId: string, values: {
-    name: string,
-}): Promise<void | Error> {
-    return firestore()
-        .doc('tasks/' + taskId)
-        .update({
-            subtasks: firestore.FieldValue.arrayUnion({
-                id: nanoid(),
-                isDone: false,
-                parentId: taskId,
-                createdAt: Date.now(),
-                name: values.name.trim(),
-            }),
-        })
+// TODO: is this function ever used? Remove it?
+export function createTask(values: {
+  name: string;
+  userId: string;
+}): Promise<firestore.DocumentReference> {
+  return firestore()
+    .collection('tasks')
+    .add({
+      ...values,
+      isDone: false,
+      dueAt: subtractDays(new Date(), 1).getTime(),
+    });
+}
+
+export function createSubtask(
+  taskId: string,
+  values: {
+    name: string;
+  },
+): Promise<void | Error> {
+  return firestore()
+    .doc('tasks/' + taskId)
+    .update({
+      subtasks: firestore.FieldValue.arrayUnion({
+        id: nanoid(),
+        isDone: false,
+        parentId: taskId,
+        createdAt: Date.now(),
+        name: values.name.trim(),
+      }),
+    });
 }
 
 interface SubtaskType {
-    id: string,
-    isDone: boolean,
-    parentId: string,
-    createdAt: string,
-    name: string,
+  id: string;
+  isDone: boolean;
+  parentId: string;
+  createdAt: string;
+  name: string;
 }
 
-export async function updateSubtask(subtask: SubtaskType, values: {
-    name: string,
-    doneAt: string,
-    isDone: boolean,
-}, ): Promise<void | Error> {
-    const docRef = firestore().doc('tasks/' + subtask.parentId)
-    const task: any = await docRef.get()
-    const newSubtasks: any[] = task.data()
-        .subtasks
-        .map((i: SubtaskType) => {
-            return i.id === subtask.id
-                ? Object.assign({}, i, values)
-                : i
-        })
-    return docRef.update({
-        subtasks: newSubtasks
-    })
+export async function updateSubtask(
+  subtask: SubtaskType,
+  values: {
+    name: string;
+    doneAt: string;
+    isDone: boolean;
+  },
+): Promise<void | Error> {
+  const docRef = firestore().doc('tasks/' + subtask.parentId);
+  const task: any = await docRef.get();
+  const newSubtasks: any[] = task
+    .data()
+    .subtasks.map((i: SubtaskType) => {
+      return i.id === subtask.id ? Object.assign({}, i, values) : i;
+    });
+  return docRef.update({
+    subtasks: newSubtasks,
+  });
 }
 
-export function deleteSubtask(taskId: string, subtask: {
-    id: string,
-}): Promise<void | Error> {
-    return firestore()
-        .doc('tasks/' + taskId)
-        .update({
-            subtasks: firestore.FieldValue.arrayRemove(subtask),
-        })
+export function deleteSubtask(
+  taskId: string,
+  subtask: {
+    id: string;
+  },
+): Promise<void | Error> {
+  return firestore()
+    .doc('tasks/' + taskId)
+    .update({
+      subtasks: firestore.FieldValue.arrayRemove(subtask),
+    });
 }

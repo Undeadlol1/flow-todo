f20df9755328d0411326c97c7d6eb3e823b9bc70
Mihail Paley <undeadlol1@mail.ru>
2019-11-13 12:23:24 +0500

    (TasksList) add "canDelete" property

--- src/components/tasks/TasksList/TasksList.js
+++ src/components/tasks/TasksList/TasksList.js
@@ -15,6 +15,7 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import subtractHours from 'date-fns/subHours';
 import Paper from '@material-ui/core/Paper';
 import { useTranslation } from 'react-i18next';
+import { When } from 'react-if';
 
 const useStyles = makeStyles(theme => {
   const color = theme.palette.text.primary;
@@ -32,7 +33,9 @@ const useStyles = makeStyles(theme => {
   };
 });
 
-export function TasksList({ loading, tasks, deleteTask }) {
+export function TasksList({
+ loading, tasks, canDelete, deleteTask 
+}) {
   const [t] = useTranslation();
   const classes = useStyles();
   if (loading) return null;
@@ -49,15 +52,17 @@ export function TasksList({ loading, tasks, deleteTask }) {
             to={`/tasks/${task.id}`}
           >
             <ListItemText primary={task.data().name} />
-            <ListItemSecondaryAction>
-              <IconButton
-                edge="end"
-                aria-label="Delete"
-                onClick={() => deleteTask(task.id)}
-              >
-                <DeleteIcon />
-              </IconButton>
-            </ListItemSecondaryAction>
+            <When condition={canDelete}>
+              <ListItemSecondaryAction>
+                <IconButton
+                  edge="end"
+                  aria-label="Delete"
+                  onClick={() => deleteTask(task.id)}
+                >
+                  <DeleteIcon />
+                </IconButton>
+              </ListItemSecondaryAction>
+            </When>
           </ListItem>
         ))}
       </List>
@@ -68,11 +73,13 @@ export function TasksList({ loading, tasks, deleteTask }) {
 TasksList.defaultProps = {
   tasks: null,
   loading: false,
+  canDelete: false,
 };
 
 TasksList.propTypes = {
   loading: PropTypes.bool,
   tasks: PropTypes.object,
+  canDelete: PropTypes.bool,
   deleteTask: PropTypes.func.isRequired,
 };
 
@@ -82,10 +89,11 @@ export default function TasksListContainer(props) {
   const db = firestore().collection('tasks');
   const [user, userLoading, userError] = useAuthState(auth());
   const [tasks, tasksLoading, tasksError] = useCollection(
-    user && db
-      .where('userId', '==', user && user.uid)
-      .where('isDone', '==', true)
-      .where('doneAt', '>', lastSixteenHours),
+    user
+      && db
+        .where('userId', '==', user && user.uid)
+        .where('isDone', '==', true)
+        .where('doneAt', '>', lastSixteenHours),
   );
 
   if (userError || tasksError) console.error(userError || tasksError);

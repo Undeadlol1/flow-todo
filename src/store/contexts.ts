b5ec2e7b6fb5c563c99e7086fdcaea5020472e8d
Mihail Paley <undeadlol1@mail.ru>
2019-11-17 19:30:30 +0500

    (contexts) add props

--- src/store/contexts.ts
+++ src/store/contexts.ts
@@ -1,5 +1,13 @@
 import { createContext } from 'react';
+import { ITask } from './index';
+import { firestore } from 'firebase';
 
-export const TasksContext = createContext({
+type ContextProps = {
+  tasks: firestore.QuerySnapshot;
+  loading: boolean;
+};
+
+export const TasksContext = createContext(<Partial<ContextProps>>{
+  tasks: {},
   loading: true,
 });

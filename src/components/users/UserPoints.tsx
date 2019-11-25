bde6e4901bba33b7e5f62e1685d8d9b60463e0dd
Mihail Paley <undeadlol1@mail.ru>
2019-11-26 01:14:01 +0500

    (UserPoints) fix values calculation

--- src/components/users/UserPoints.tsx
+++ src/components/users/UserPoints.tsx
@@ -2,20 +2,29 @@ import Chip from '@material-ui/core/Chip';
 import React, { memo } from 'react';
 import CountUp from 'react-countup';
 import usePrevious from 'react-use/esm/usePrevious';
+import debug from 'debug';
 
-const UserPoints = memo(({ value: newValue }: { value: number }) => {
-  const oldValue = usePrevious(newValue);
-  return (
-    <Chip
-      color="secondary"
-      label={
-        <CountUp
-          end={newValue}
-          start={oldValue ? newValue - oldValue : 0}
-        />
-      }
-    />
-  );
-});
+const logger = debug('UserPoints');
+
+const UserPoints = memo(
+  ({ value: nextPoints }: { value: number }) => {
+    const previousPoints = usePrevious(nextPoints) || 0;
+    const difference = nextPoints - previousPoints;
+    logger('previousPoints', previousPoints);
+    logger('nextPoints', nextPoints);
+    logger('difference', difference);
+    return (
+      <Chip
+        color="secondary"
+        label={
+          <CountUp
+            end={nextPoints}
+            start={previousPoints && nextPoints - difference}
+          />
+        }
+      />
+    );
+  },
+);
 
 export default UserPoints;

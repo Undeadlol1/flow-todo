e9ee97fe91ca5646a2979752b0a49ab54eb6972c
Mihail Paley <undeadlol1@mail.ru>
2019-11-28 10:57:56 +0500

    disable service worker

--- src/index.tsx
+++ src/index.tsx
@@ -8,5 +8,8 @@ ReactDOM.render(<App />, document.getElementById('root'));
 
 // Make app work offline and load faster.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-if (process.env.NODE_ENV === 'production') serviceWorker.register();
-else serviceWorker.unregister();
+// NOTE: disabled because project doesn't need to be
+// cached so agressively during active development stage
+// if (process.env.NODE_ENV === 'production') serviceWorker.register();
+// else serviceWorker.unregister();
+serviceWorker.unregister();

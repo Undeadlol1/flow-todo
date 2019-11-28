3d702f313dd9cb5a63fd929da8c017ed1f51ebcd
Mihail Paley <undeadlol1@mail.ru>
2019-11-28 21:11:51 +0500

    (TaskPageCointainer) refactoring

--- src/pages/TaskPage/TaskPageContainer.tsx
+++ src/pages/TaskPage/TaskPageContainer.tsx
@@ -1,24 +1,25 @@
+import { firestore } from 'firebase/app';
+import get from 'lodash/get';
+import random from 'lodash/random';
+import { snackbarActions } from 'material-ui-snackbar-redux';
+import { useSnackbar } from 'notistack';
+import React, { memo, useState } from 'react';
 import { useDocumentData } from 'react-firebase-hooks/firestore';
 import { useTranslation } from 'react-i18next';
+import { useDispatch } from 'react-redux';
 import { useHistory, useParams } from 'react-router-dom';
 import {
   calculateNextRepetition,
   handleErrors,
 } from '../../services';
 import { deleteTask, updateSubtask } from '../../store';
-import { TasksContext } from '../../store/contexts';
-import { Subtask, Task, addPoints } from '../../store/index';
-import React, { useContext, useState, memo } from 'react';
-// import random from 'lodash/random';
-import { useSnackbar } from 'notistack';
-import { firestore } from 'firebase/app';
-import find from 'lodash/find';
-import invoke from 'lodash/invoke';
+import {
+  addPoints,
+  Subtask,
+  Task,
+  useTypedSelector,
+} from '../../store/index';
 import TaskPage from './TaskPage';
-import get from 'lodash/get';
-import { useDispatch } from 'react-redux';
-import { snackbarActions } from 'material-ui-snackbar-redux';
-import random from 'lodash/random';
 
 function getRandomTaskId(tasks: Task[]): string {
   return get(tasks, `[${random(tasks.length - 1)}].id`);
@@ -30,29 +31,26 @@ export default memo(() => {
   const dispatch = useDispatch();
   const { enqueueSnackbar } = useSnackbar();
   const [isRequested, setRequested] = useState();
-  // TODO this needs to be a service
 
   const { taskId = '' } = useParams();
   const isAppIntroMode = taskId === 'introExample';
-  const taskPointer = firestore()
-    .collection('tasks')
-    .doc(taskId);
-  const { tasks } = useContext(TasksContext);
-
-  // TODO: this is a mess. Rework this
-  const currentTask = invoke(
-    // @ts-ignore
-    get<Task[]>(tasks, 'docs', []).find((t: Task) =>
-      // @ts-ignore
-      t.get('isCurrent'),
-    ),
-    'data',
+  const activeTasks =
+    useTypedSelector<Task[]>(
+      ({ firestore }) => firestore.ordered.activeTasks,
+    ) || [];
+  const { requested } = useTypedSelector(
+    ({ firestore }) => firestore.status,
   );
+  const currentTask = activeTasks.find(t => t.isCurrent);
   const nextTaskId = getRandomTaskId(
-    // @ts-ignore
-    get(tasks, 'docs', []).filter((t: any) => !t.isCurrent),
+    activeTasks.filter((t: any) => !t.isCurrent),
   );
 
+  if (get(requested, 'activeTasks') && currentTask!.id !== taskId) {
+    // TODO: load task data
+    // return;
+  }
+  const taskPointer = firestore().doc('tasks/' + taskId);
   let [task, taskLoading, taskError] = useDocumentData(
     // @ts-ignore
     !isAppIntroMode || !currentTask ? taskPointer : undefined,
@@ -70,10 +68,9 @@ export default memo(() => {
     async deleteTask() {
       history.push('/');
       try {
-        const task = find(get(tasks, 'docs'), ['id', taskId]);
         await Promise.all([
           deleteTask(taskId),
-          addPoints(task!.get('userId'), 10),
+          addPoints(task.userId, 10),
         ]);
         dispatch(
           snackbarActions.showMessage(

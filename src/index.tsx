e8d1b49089981ccc2ab83fcf60cb754defe501df
Mihail Paley <undeadlol1@mail.ru>
2019-11-29 18:13:37 +0500

    Revert "disable service worker"
    This reverts commit e9ee97fe91ca5646a2979752b0a49ab54eb6972c.

--- src/index.tsx
+++ src/index.tsx
@@ -8,8 +8,5 @@ ReactDOM.render(<App />, document.getElementById('root'));
 
 // Make app work offline and load faster.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-// NOTE: disabled because project doesn't need to be
-// cached so agressively during active development stage
-// if (process.env.NODE_ENV === 'production') serviceWorker.register();
-// else serviceWorker.unregister();
-serviceWorker.unregister();
+if (process.env.NODE_ENV === 'production') serviceWorker.register();
+else serviceWorker.unregister();

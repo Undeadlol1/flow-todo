6d05e69b3612219e075784a416659a4d691df25b
Mihail Paley <undeadlol1@mail.ru>
2019-12-05 00:13:06 +0500

    initialize i18n before running any tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,10 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import 'jest-enzyme';
+import { initializeI18n } from './App';
 
 Enzyme.configure({ adapter: new Adapter() });
+
+beforeAll(() => {
+  initializeI18n();
+});

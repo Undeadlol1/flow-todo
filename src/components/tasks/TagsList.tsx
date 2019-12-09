3aec526c84245468f7cdd1f40af1fd64ae0c68ab
Mihail Paley <undeadlol1@mail.ru>
2019-12-09 23:36:39 +0500

    (WIP) add simple TagsList

---
+++ src/components/tasks/TagsList.tsx
@@ -0,0 +1,36 @@
+import React, { memo } from 'react';
+import { useTypedSelector, Task } from '../../store/index';
+import Box from '@material-ui/core/Box';
+import debug from 'debug';
+import isEmpty from 'lodash/isEmpty';
+import uniq from 'lodash/uniq';
+import Button from '@material-ui/core/Button';
+
+const log = debug('TagsList');
+debug.enable('TagsList');
+
+export const TagsList: React.FC<{}> = () => {
+  const { activeTasks }: { activeTasks: Task[] } = useTypedSelector(
+    s => s.firestore.ordered,
+  );
+
+  let tags: string[] = [];
+  (activeTasks || [])
+    .filter(t => !isEmpty(t.tags))
+    .forEach(t => {
+      (t.tags as string[]).forEach(tag => tags.push(tag));
+    });
+  tags = uniq(tags);
+  log('tags.length: ', tags.length);
+
+  if (!activeTasks) return null;
+  return (
+    <Box>
+      {tags.map(tag => (
+        <Button key={tag}>{tag}</Button>
+      ))}
+    </Box>
+  );
+};
+
+export default memo(TagsList);

dcb59d1c155f7585ce558d9db38302dc15c29701
Mihail Paley <undeadlol1@mail.ru>
2019-12-11 00:50:19 +0500

    make SubtasksList sortable

--- src/components/tasks/SubtasksList.tsx
+++ src/components/tasks/SubtasksList.tsx
@@ -1,22 +1,25 @@
-import React from 'react';
-import { auth } from 'firebase/app';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-// import ListItemIcon from '@material-ui/core/ListItemIcon';
-// import Checkbox from '@material-ui/core/Checkbox';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListSubheader from '@material-ui/core/ListSubheader';
-import IconButton from '@material-ui/core/IconButton';
-import DeleteIcon from '@material-ui/icons/Delete';
 import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+import DeleteIcon from '@material-ui/icons/Delete';
+import DragHandleIcon from '@material-ui/icons/DragHandle';
 import isEmpty from 'lodash/isEmpty';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
-import reverse from 'lodash/reverse';
+import {
+  SortableContainer,
+  SortableElement,
+  SortableHandle,
+} from 'react-sortable-hoc';
 import { deleteSubtask, Subtask } from '../../store';
-import { handleErrors } from '../../services/index';
+import arrayMove from 'array-move';
+import { firestore } from 'firebase/app';
 
 const useStyles = makeStyles(theme => {
   const color = theme.palette.text.primary;
@@ -39,55 +42,73 @@ interface Props {
   documents?: Subtask[];
 }
 
+const DragHandle = SortableHandle(() => (
+  <ListItemIcon style={{ cursor: 'grab' }}>
+    <DragHandleIcon />
+  </ListItemIcon>
+));
+
+const SortableItem = SortableElement(
+  ({ subtask }: { subtask: Subtask }) => {
+    const classes = useStyles();
+    return (
+      <ListItem ContainerComponent="div">
+        <DragHandle />
+        <ListItemText
+          primary={subtask.name}
+          className={classes.link}
+        />
+        <ListItemSecondaryAction>
+          <IconButton
+            edge="end"
+            aria-label="Delete"
+            onClick={() => deleteSubtask(subtask.parentId, subtask)}
+          >
+            <DeleteIcon />
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
+    );
+  },
+);
+
+const SortableListContainer = SortableContainer(
+  ({ items }: { items: Subtask[] }) => {
+    const [t] = useTranslation();
+    const classes = useStyles();
+    return (
+      <List className={classes.list}>
+        <ListSubheader>{`${t('subtasks')}:`}</ListSubheader>
+        {items.map((i, index) => (
+          <SortableItem key={i.id} index={index} subtask={i} />
+        ))}
+      </List>
+    );
+  },
+);
+
 export default function SubtasksList({ documents, ...props }: Props) {
-  const [t] = useTranslation();
   const classes = useStyles();
-  const [user, userIsLoading, userError] = useAuthState(auth());
-  const isDisabled = !!(userIsLoading || userError || !user);
-
-  handleErrors(userError);
-
-  /* TODO: toggleDone must act as "setDone" in TaskChoices */
-  // function toggleIsDone(subtask) {
-  //   updateSubtask(subtask, {
-  //     isDone: !subtask.isDone,
-  //     doneAt: subtask.isDone ? null : Date.now(),
-  //   })
-  //   .catch(error => console.error(error));
-  // }
 
   if (!documents || isEmpty(documents)) return null;
 
+  const onSortEnd = ({ oldIndex, newIndex }: any) => {
+    firestore()
+      .collection('tasks')
+      .doc(documents[0].parentId)
+      .update({
+        subtasks: arrayMove(documents, oldIndex, newIndex),
+      });
+  };
+
   return (
     <Paper elevation={6} className={classes.paper}>
-      <List className={classes.list}>
-        <ListSubheader>{`${t('subtasks')}:`}</ListSubheader>
-        {reverse(documents).map(task => (
-          <ListItem key={task.id} className={classes.link}>
-            {/* <ListItemIcon>
-              <Checkbox
-                disableRipple
-                edge="start"
-                tabIndex={-1}
-                checked={task.isDone}
-                inputProps={{ 'aria-labelledby': `checkbox-list-label-${task.name}` }}
-                onClick={() => toggleIsDone(task)}
-              />
-            </ListItemIcon> */}
-            <ListItemText primary={task.name} />
-            <ListItemSecondaryAction>
-              <IconButton
-                edge="end"
-                aria-label="Delete"
-                disabled={isDisabled}
-                onClick={() => deleteSubtask(task.parentId, task)}
-              >
-                <DeleteIcon />
-              </IconButton>
-            </ListItemSecondaryAction>
-          </ListItem>
-        ))}
-      </List>
+      <SortableListContainer
+        lockAxis="y"
+        items={documents}
+        onSortEnd={onSortEnd}
+        useDragHandle={true}
+      />
     </Paper>
   );
 }

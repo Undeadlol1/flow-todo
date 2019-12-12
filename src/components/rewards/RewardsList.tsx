4a65d513d33d4ce11d55e101f73fff77b2c35437
Mihail Paley <undeadlol1@mail.ru>
2019-12-12 05:19:44 +0500

    add Skeleton to RewardsList to improve loading animation

--- src/components/rewards/RewardsList.tsx
+++ src/components/rewards/RewardsList.tsx
@@ -1,21 +1,39 @@
 import isUndefined from 'lodash/isUndefined';
 import React, { memo } from 'react';
 import { Reward } from '../../store/rewardsSlice';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import Box from '@material-ui/core/Box';
+import Skeleton from '@material-ui/lab/Skeleton';
+import nanoid from 'nanoid';
+import isNull from 'lodash/isNull';
 
 interface Props {
   values?: Reward[];
 }
 
 const RewardsList: React.FC<Props> = ({ values: rewards }) => {
-  if (isUndefined(rewards)) return null;
+  if (isNull(rewards)) return null;
+  if (isUndefined(rewards))
+    return (
+      <Box width="100%">
+        {Array.from({ length: 4 }, () => (
+          <Box my={2} key={nanoid()}>
+            <Skeleton width="100%" height="200px" variant="rect" />
+          </Box>
+        ))}
+      </Box>
+    );
   else
     return (
       <div>
-        <ul>
-          {rewards.map(r => (
-            <li key={r.id}>{r.name}</li>
-          ))}
-        </ul>
+        {rewards.map(r => (
+          <Box key={r.id} my={2}>
+            <Card key={r.id}>
+              <CardHeader title={r.name} subheader={r.points} />
+            </Card>
+          </Box>
+        ))}
       </div>
     );
 };

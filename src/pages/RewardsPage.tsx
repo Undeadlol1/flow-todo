2c2da0911dbeeb6be45b68c8079bc3afcd87e8b2
Mihail Paley <undeadlol1@mail.ru>
2019-12-13 06:48:35 +0500

    move rewards fetching to Router

--- src/pages/RewardsPage.tsx
+++ src/pages/RewardsPage.tsx
@@ -1,34 +1,30 @@
-import React from 'react';
-import Grid from '@material-ui/core/Grid';
-import RewardsList from '../components/rewards/RewardsList';
-import CreateReward from '../components/rewards/CreateReward';
-import { useTypedSelector } from '../store/index';
-import get from 'lodash/get';
-import { useFirestoreConnect } from 'react-redux-firebase';
-import { Reward } from '../store/rewardsSlice';
 import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import React, { memo } from 'react';
+import CreateReward from '../components/rewards/CreateReward';
+import RewardsList from '../components/rewards/RewardsList';
+import { useTypedSelector } from '../store/index';
+import { Reward } from '../store/rewardsSlice';
 
 interface Props {}
 
 const RewardsPage: React.FC<Props> = () => {
-  const userId = useTypedSelector(s =>
-    get(s, 'firebase.auth.uid', ''),
-  );
   const rewards = useTypedSelector(
     s => s.firestore.ordered.rewards as Reward[],
   );
-  useFirestoreConnect([
-    {
-      collection: 'rewards',
-      where: [['userId', '==', userId]],
-      // where: [['userId', '==', userId], ['isDone', '==', false]],
-    },
-  ]);
 
   return (
     <Box>
       <Grid container justify="center" spacing={2}>
-        <Grid item xs={12} sm={8} md={8} lg={6}>
+        <Grid
+          component={Box}
+          mt={2}
+          item
+          xs={12}
+          sm={8}
+          md={8}
+          lg={6}
+        >
           <CreateReward />
         </Grid>
       </Grid>
@@ -41,4 +37,4 @@ const RewardsPage: React.FC<Props> = () => {
   );
 };
 
-export default RewardsPage;
+export default memo(RewardsPage);

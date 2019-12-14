8031ce49da32dc478ae88e44f755feb1e5335bce
Mihail Paley <undeadlol1@mail.ru>
2019-12-14 07:53:59 +0500

    add login/logout buttons to Sidebar

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -1,19 +1,32 @@
-import React, { memo } from 'react';
+import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import Drawer from '@material-ui/core/Drawer';
-import { makeStyles, withStyles } from '@material-ui/core/styles';
 import ListItemText from '@material-ui/core/ListItemText';
-import { useTypedSelector } from '../../store/index';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import debug from 'debug';
-import { useDispatch } from 'react-redux';
-import { toggleSidebar } from '../../store/uiSlice';
-import { Link } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
+import get from 'lodash/get';
+import React, { memo } from 'react';
 import MailTo from 'react-mailto.js';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import {
+  useTypedTranslate,
+  handleErrors,
+} from '../../services/index';
+import { useTypedSelector } from '../../store/index';
+import { toggleSidebar } from '../../store/uiSlice';
+import { getFirebase } from 'react-redux-firebase';
+import { auth } from 'firebase/app';
+import { When, Unless } from 'react-if';
+import isUndefined from 'lodash/isUndefined';
 
 const log = debug('Sidebar');
 const useStyles = makeStyles({
+  link: {
+    width: '100%',
+    color: 'inherit',
+    textDecoration: 'none',
+  },
   list: {
     width: '250px',
   },
@@ -32,9 +45,23 @@ const StyledListText = withStyles({
 
 const Sidebar: React.FC<{}> = () => {
   const cx = useStyles();
-  const [t] = useTranslation();
+  const t = useTypedTranslate();
   const dispatch = useDispatch();
   const { isSidebarOpen } = useTypedSelector(s => s.ui);
+  // @ts-ignore
+  const auth = useTypedSelector(s => s.firebase.auth);
+  const isAnonymous = useTypedSelector(s =>
+    get(s, 'firebase.auth.isAnonymous'),
+  );
+  function logoutIfNeeded() {
+    console.log('logoutIfNeeded: ');
+    if (!isAnonymous)
+      getFirebase()
+        .logout()
+        .then(() => console.log('logged user out'))
+        .catch(handleErrors);
+  }
+  log('isAnonymous: ', isAnonymous);
   log('isSidebarOpen: ', isSidebarOpen);
   return (
     <Drawer
@@ -50,6 +77,17 @@ const Sidebar: React.FC<{}> = () => {
             <StyledListText primary={t('feedback')} />
           </ListItem>
         </MailTo>
+        <Unless condition={isUndefined(isAnonymous)}>
+          <ListItem button onClick={logoutIfNeeded}>
+            {isAnonymous ? (
+              <Link to="/signin" className={cx.link}>
+                <StyledListText primary={t('log in')} />
+              </Link>
+            ) : (
+              <StyledListText primary={t('log out')} />
+            )}
+          </ListItem>
+        </Unless>
       </List>
     </Drawer>
   );

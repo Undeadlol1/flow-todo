07b38bf61ef5fd1abd2a57f4b2c710a3dde7539b
Mihail Paley <undeadlol1@mail.ru>
2019-12-21 17:15:35 +0500

    (PinnedTask) refactor

--- src/components/tasks/TasksList/PinnedTask.tsx
+++ src/components/tasks/TasksList/PinnedTask.tsx
@@ -5,13 +5,19 @@ import { useSelector } from 'react-redux';
 import { pinnedTaskSelector } from '../../../store/selectors';
 import CreateSubtask from '../CreateSubtask/CreateSubtask';
 import SubtasksList from '../SubtasksList';
+import debug from 'debug';
+import isEmpty from 'lodash/isEmpty';
 
 let taskId = '';
+const log = debug('Pinnedtask');
+debug.enable('PinnedTask');
 
 export default memo(function PinnedTask() {
   const task = useSelector(pinnedTaskSelector);
-  if (!task) return null;
+  log('task: ', task);
+  if (isEmpty(task)) return null;
   if (task && task.id) taskId = task.id;
+  log('taskId: ', taskId);
   return (
     <Card>
       <CardHeader title={task.name} />

03fac6d3466bff84fd951c888bc1ccb1099c5e3c
Mihail Paley <undeadlol1@mail.ru>
2019-12-25 10:05:52 +0500

    (SubtasksList) add "done" button

--- src/components/tasks/SubtasksList.tsx
+++ src/components/tasks/SubtasksList.tsx
@@ -7,6 +7,7 @@ import ListItemText from '@material-ui/core/ListItemText';
 import ListSubheader from '@material-ui/core/ListSubheader';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
+import CheckBoxIcon from '@material-ui/icons/CheckBoxOutlineBlank';
 import DeleteIcon from '@material-ui/icons/Delete';
 import DragHandleIcon from '@material-ui/icons/DragHandle';
 import arrayMove from 'array-move';
@@ -14,14 +15,21 @@ import firebase from 'firebase/app';
 import isEmpty from 'lodash/isEmpty';
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
 import {
   SortableContainer,
   SortableElement,
   SortableHandle,
 } from 'react-sortable-hoc';
 import { getFirestore } from 'redux-firestore';
+import {
+  handleErrors,
+  showSnackbar,
+  useTypedTranslate,
+} from '../../services/index';
 import { deleteSubtask, Subtask } from '../../store';
-import { handleErrors } from '../../services/index';
+import { addPointsWithSideEffects } from '../../store/index';
+import { authSelector } from '../../store/selectors';
 
 const useStyles = makeStyles(theme => {
   const color = theme.palette.text.primary;
@@ -52,20 +60,36 @@ const DragHandle = SortableHandle(() => (
 
 const SortableItem = SortableElement(
   ({ subtask }: { subtask: Subtask }) => {
+    const t = useTypedTranslate();
+    const auth = useSelector(authSelector);
     const classes = useStyles();
+
+    function remove() {
+      return deleteSubtask(subtask.parentId, subtask);
+    }
+
+    function setDone() {
+      showSnackbar(t('goodJobPointsRecieved', { points: 10 }));
+      Promise.all([
+        remove(),
+        addPointsWithSideEffects(auth.uid, 10),
+      ]).catch(handleErrors);
+    }
+
     return (
       <ListItem ContainerComponent="div">
         <DragHandle />
+        <ListItemIcon>
+          <IconButton edge="end" aria-label="Done" onClick={setDone}>
+            <CheckBoxIcon />
+          </IconButton>
+        </ListItemIcon>
         <ListItemText
           primary={subtask.name}
           className={classes.link}
         />
         <ListItemSecondaryAction>
-          <IconButton
-            edge="end"
-            aria-label="Delete"
-            onClick={() => deleteSubtask(subtask.parentId, subtask)}
-          >
+          <IconButton edge="end" onClick={remove} aria-label="Delete">
             <DeleteIcon />
           </IconButton>
         </ListItemSecondaryAction>

51de482388e298e1222500bbfb7a634db7b0da5b
Mihail Paley <undeadlol1@mail.ru>
2020-03-10 20:52:55 +0500

    add few questions to FAQPage

--- src/pages/FAQPage.tsx
+++ src/pages/FAQPage.tsx
@@ -2,6 +2,10 @@ import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import React, { memo } from 'react';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const useStyles = makeStyles(theme => ({
   pageContainer: {
@@ -11,9 +15,16 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
+const data = [
+  ['Что это за приложение?', 'Тест'],
+  ['Зачем оно нужно?', ''],
+  ['Какие задачи решает?', ''],
+  ['Где все мои задачи?', ''],
+  ['Что значит "аноним"?', ''],
+];
+
 const FAQPage = memo(function FAQPage() {
   const classes = useStyles();
-  //   const t = useTypedTranslate();
 
   return (
     <Grid
@@ -25,9 +36,26 @@ const FAQPage = memo(function FAQPage() {
       className={classes.pageContainer}
     >
       <Grid item xs={12} sm={6}>
-        <Typography align="center" component="h1">
+        {/* TODO: i18n */}
+        <Typography align="center" variant="h1">
           FAQ
         </Typography>
+        {data.map(i => (
+          <ExpansionPanel>
+            <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography variant="subtitle1" component="h2">
+                {i[0]}
+              </Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Typography>{i[1]}</Typography>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+        ))}
       </Grid>
     </Grid>
   );

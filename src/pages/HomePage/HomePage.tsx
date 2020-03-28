95ae7ed4f1cb8e7041b7511f2b0647f8fb489aff
Mihail Paley <undeadlol1@mail.ru>
2020-03-28 16:17:04 +0500

    (HomePage) render TasksDoneToday conditionally

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -16,6 +16,7 @@ import clsx from 'clsx';
 import { useScreenIsNarrow } from '../../services/index';
 import { TagsList } from '../../components/tasks/TagsList';
 import TasksDoneToday from '../../components/tasks/TasksDoneToday';
+import { Unless } from 'react-if';
 
 const log = debug('HomePage');
 const useStyles = makeStyles(theme => ({
@@ -60,12 +61,7 @@ export const HomePage = memo(function HomePage(props: Props) {
       isAppTourActive ||
       !isEmpty(createdAtleastOneTask)
     )
-      return (
-        <>
-          <TasksDoneToday />
-          <GetRandomTask className={'IntroHandle__taskButton'} />;
-        </>
-      );
+      return <GetRandomTask className={'IntroHandle__taskButton'} />;
     else return <WelcomeCard />;
   }
 
@@ -79,6 +75,15 @@ export const HomePage = memo(function HomePage(props: Props) {
       alignContent="center"
       className={classes.pageContainer}
     >
+      <Grid item xs={12} sm={12} md={8} lg={6}>
+        <Unless
+          condition={Boolean(
+            isEmpty(createdAtleastOneTask) || isAppTourActive,
+          )}
+        >
+          <TasksDoneToday />
+        </Unless>
+      </Grid>
       <Grid item xs={12} sm={12} md={8} lg={6}>
         <PinnedTask />
       </Grid>

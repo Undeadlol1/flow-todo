1950f7e1537df052d3974af3ae025a15545f8ef3
Mihail Paley <undeadlol1@mail.ru>
2020-03-30 17:47:32 +0500

    (Theme) apply memoization

--- src/Theme.tsx
+++ src/Theme.tsx
@@ -9,39 +9,41 @@ import { useSelector } from 'react-redux';
 import { uiSelector } from './store/selectors';
 import { ruRU, enUS } from '@material-ui/core/locale/';
 
-export const Theme: FunctionComponent<{}> = ({ children }) => {
-  const uiState = useSelector(uiSelector);
-  const { language } = i18n;
-  const theme = React.useMemo(
-    () =>
-      createMuiTheme(
-        {
-          palette: {
-            primary: { main: '#81D4FA' },
-            secondary: {
-              main: '#00838F',
-              contrastText: '#ffffff',
+export const Theme: FunctionComponent<{}> = React.memo(
+  ({ children }) => {
+    const uiState = useSelector(uiSelector);
+    const { language } = i18n;
+    const theme = React.useMemo(
+      () =>
+        createMuiTheme(
+          {
+            palette: {
+              primary: { main: '#81D4FA' },
+              secondary: {
+                main: '#00838F',
+                contrastText: '#ffffff',
+              },
+              type: uiState.preferedColorScheme,
             },
-            type: uiState.preferedColorScheme,
           },
-        },
-        // @ts-ignore
-        { ru: ruRU, en: enUS }[language],
-      ),
-    [language, uiState.preferedColorScheme],
-  );
-  // Make font sizes responsive
-  // @ts-ignore
-  theme.typography.h1 = {
-    [theme.breakpoints.down('md')]: {
-      fontSize: '2.4rem',
-    },
-  };
+          // @ts-ignore
+          { ru: ruRU, en: enUS }[language],
+        ),
+      [language, uiState.preferedColorScheme],
+    );
+    // Make font sizes responsive
+    // @ts-ignore
+    theme.typography.h1 = {
+      [theme.breakpoints.down('md')]: {
+        fontSize: '2.4rem',
+      },
+    };
 
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      {children}
-    </ThemeProvider>
-  );
-};
+    return (
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    );
+  },
+);

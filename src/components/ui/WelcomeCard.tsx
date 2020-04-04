ee880c07e5629e11cb2858ddb2512dd7c7f9b9e4
Mihail Paley <undeadlol1@mail.ru>
2020-04-04 11:42:57 +0500

    remove comments, clean up

--- src/components/ui/WelcomeCard.tsx
+++ src/components/ui/WelcomeCard.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
-// import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-// import { useDispatch } from 'react-redux';
-// import { toggleAppTour } from '../../store/uiSlice';
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -15,15 +12,9 @@ const useStyles = makeStyles(theme => ({
     display: 'block',
     paddingBottom: theme.spacing(3),
   },
-  button: {
-    margin: '0 auto',
-    display: 'block',
-  },
 }));
 
-interface Props {}
-
-const WelcomeCard: React.FC<Props> = () => {
+const WelcomeCard: React.FC<{}> = () => {
   const classes = useStyles();
   return (
     <Paper className={classes.container} elevation={6}>
@@ -38,18 +29,6 @@ const WelcomeCard: React.FC<Props> = () => {
         психологических трюков облегчает задачи и позволяет сдвинуться
         с мертвой точки.
       </Typography>
-      {/* WIP */}
-      {/* <Typography paragraph>
-        Долгий Ящик - это приложение которое позволит справиться с этими
-        проблемами с помощью трех простых шагов.
-      </Typography>
-      <Button
-        className={classes.button}
-        variant="outlined"
-        onClick={() => dispatch(toggleAppTour())}
-      >
-        хотите узнать как?
-      </Button> */}
     </Paper>
   );
 };

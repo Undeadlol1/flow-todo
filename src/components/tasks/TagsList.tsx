f73335570619969bdc32270fde57e54d0bf50d47
Mihail Paley <undeadlol1@mail.ru>
2020-04-05 15:23:13 +0500

    (TagsList) use activeTags instead of excludedTags

--- src/components/tasks/TagsList.tsx
+++ src/components/tasks/TagsList.tsx
@@ -1,47 +1,45 @@
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import { useTheme } from '@material-ui/core/styles';
-import debug from 'debug';
 import React, { memo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
-  excludedTagsSelector,
   fetchedTasksSelector,
   tagsOfFetchedTasksSelector,
 } from '../../store/selectors';
 import { excludeTag, includeTag } from '../../store/tasksSlice';
-
-const log = debug('TagsList');
+import { activeTagsSelector } from '../../store/selectors';
 
 export const TagsList: React.FC<{}> = () => {
   const theme = useTheme();
   const dispatch = useDispatch();
 
   const tasks = useSelector(fetchedTasksSelector);
-  const excludedTags = useSelector(excludedTagsSelector);
+  const activeTags = useSelector(activeTagsSelector);
   const uniqueTags = useSelector(tagsOfFetchedTasksSelector);
 
   if (!tasks) return null;
+  else
+    return (
+      <Box>
+        {uniqueTags.map(tag => {
+          const isActive = activeTags.includes(tag);
+          const color = isActive
+            ? 'inherit'
+            : theme.palette.grey.A700;
 
-  log('uniqueTags.length', uniqueTags.length);
+          function toggleTag() {
+            dispatch(isActive ? excludeTag(tag) : includeTag(tag));
+          }
 
-  return (
-    <Box>
-      {uniqueTags.map(tag => {
-        const isActive = !excludedTags.includes(tag);
-        const color = isActive ? 'inherit' : theme.palette.grey.A700;
-        function toggleTag() {
-          dispatch(isActive ? excludeTag(tag) : includeTag(tag));
-        }
-
-        return (
-          <Button key={tag} onClick={toggleTag} style={{ color }}>
-            {tag}
-          </Button>
-        );
-      })}
-    </Box>
-  );
+          return (
+            <Button key={tag} onClick={toggleTag} style={{ color }}>
+              {tag}
+            </Button>
+          );
+        })}
+      </Box>
+    );
 };
 
 export default memo(TagsList);

505435a5ca4ffbcf724eadb2316a2999b3245afe
Mihail Paley <undeadlol1@mail.ru>
2020-06-15 19:54:11 +0500

    move snackbar up on mobile devices

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 // import { useWindowSize } from '@reach/window-size';
+import { createMuiTheme } from '@material-ui/core/styles';
 import firebase from 'firebase/app';
 import { SnackbarProvider as MaterialSnackbarProvider } from 'material-ui-snackbar-redux';
 import { SnackbarProvider as NotistackSnackbarProver } from 'notistack';
 import React from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
 import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { useWindowSize } from 'react-use';
 import { createFirestoreInstance } from 'redux-firestore';
 import './App.css';
 import Router from './Router';
@@ -15,11 +17,9 @@ import { Theme } from './Theme';
 initializeI18n();
 
 function App() {
-  // const theme = React.useMemo(() => createMuiTheme(), []);
-  const isMobile = false;
-  // This is an experiment. Mobie app might look better without density.
-  // const isMobile =
-  //   useWindowSize().width < theme.breakpoints.values.sm;
+  const theme = React.useMemo(() => createMuiTheme(), []);
+  const isMobile =
+    useWindowSize().width < theme.breakpoints.values.sm;
 
   const reduxFirebaseProps = {
     firebase,
@@ -36,16 +36,24 @@ function App() {
       <ReduxProvider store={store}>
         <ReactReduxFirebaseProvider {...reduxFirebaseProps}>
           <Theme isMobile={isMobile}>
+            {/* TODO do i even use this? Remove if not */}
             <MaterialSnackbarProvider
               SnackbarProps={{ autoHideDuration: 4000 }}
             >
               <NotistackSnackbarProver
                 autoHideDuration={3500}
                 dense={isMobile}
-                anchorOrigin={{
-                  vertical: 'bottom',
-                  horizontal: 'center',
-                }}
+                anchorOrigin={
+                  isMobile
+                    ? {
+                        vertical: 'top',
+                        horizontal: 'center',
+                      }
+                    : {
+                        vertical: 'bottom',
+                        horizontal: 'center',
+                      }
+                }
               >
                 <Router />
               </NotistackSnackbarProver>
--- src/Theme.tsx
+++ src/Theme.tsx
@@ -28,44 +28,11 @@ export const Theme: FunctionComponent<{
           },
           props: isMobile
             ? {
-                MuiButton: {
-                  size: 'small',
-                },
-                MuiFilledInput: {
-                  margin: 'dense',
-                },
-                MuiFormControl: {
-                  margin: 'dense',
-                },
-                MuiFormHelperText: {
-                  margin: 'dense',
-                },
-                MuiIconButton: {
-                  size: 'medium',
-                },
-                MuiInputBase: {
-                  margin: 'dense',
-                },
-                MuiInputLabel: {
-                  margin: 'dense',
-                },
-                MuiListItem: {
-                  dense: true,
-                },
-                MuiOutlinedInput: {
-                  margin: 'dense',
-                },
-                MuiFab: {
-                  size: 'medium',
-                },
-                MuiTable: {
-                  size: 'small',
-                },
-                MuiTextField: {
-                  margin: 'dense',
-                },
-                MuiToolbar: {
-                  variant: 'dense',
+                MuiSnackbar: {
+                  anchorOrigin: {
+                    vertical: 'top',
+                    horizontal: 'center',
+                  },
                 },
               }
             : {},

39607ec71a768dea4ed48206c3e660bdf0ba7651
Mihail Paley <undeadlol1@mail.ru>
2020-06-21 22:04:12 +0500

    remove unused deps

--- src/components/tasks/TasksDoneToday.tsx
+++ src/components/tasks/TasksDoneToday.tsx
@@ -3,7 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
 import { useSelector } from 'react-redux';
 import {
   taskLogsSelector as taskLogs,

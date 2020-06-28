9d74a0eb233d0234be2c12d3ef9168db92f1724f
Mihail Paley <undeadlol1@mail.ru>
2020-06-28 16:59:06 +0500

    (TasksPage) add bottom margin

--- src/pages/TasksPage.tsx
+++ src/pages/TasksPage.tsx
@@ -10,27 +10,23 @@ import { tasksSelector } from '../store/selectors';
 const log = debug('RewardsPage');
 const useStyles = makeStyles(theme => ({
   pageContainer: {
-    marginBottom: 0,
     marginTop: theme.spacing(4),
+    marginBottom: theme.spacing(4),
     minHeight: 'calc(100vh - 74px)',
   },
 }));
 
-interface Props {}
-
-const RewardsPage: React.FC<Props> = () => {
+const RewardsPage: React.FC<{}> = () => {
   const cx = useStyles();
   const tasks = useTypedSelector(tasksSelector);
   log('tasks: %O', tasks);
 
   return (
-    <Box className={cx.pageContainer}>
-      <Grid container justify="center">
-        <Grid container item xs={12} sm={8} md={8} lg={6}>
-          <TasksList tasks={tasks || []} />
-        </Grid>
+    <Grid container justify="center" className={cx.pageContainer}>
+      <Grid container item xs={12} sm={8} md={8} lg={6}>
+        <TasksList tasks={tasks || []} />
       </Grid>
-    </Box>
+    </Grid>
   );
 };
 

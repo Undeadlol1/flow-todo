2d8ea857210e269f94325411afe33960dd0ac20b
undeadlol1 <undeadlol1@mail.ru>
2020-08-23 16:03:26 +0500

    fix: :bug: UserPoints stories missing props

--- src/components/users/UserPoints.stories.tsx
+++ src/components/users/UserPoints.stories.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import UserPoints from './UserPoints';
-import { number } from '@storybook/addon-knobs';
+import { number, boolean } from '@storybook/addon-knobs';
 import add from 'ramda/es/add';
 import { sections } from '../storybookContants';
 
@@ -14,12 +14,15 @@ export const normal = () => (
     Loading: <br />
     <UserPoints isLoaded={false} value={10} />
     Variants: <br />
-    <UserPoints value={10} /> <br />
-    <UserPoints value={100} /> <br />
-    <UserPoints value={1000} /> <br />
-    <UserPoints value={10000} /> <br />
+    <UserPoints value={10} isLoaded /> <br />
+    <UserPoints value={100} isLoaded /> <br />
+    <UserPoints value={1000} isLoaded /> <br />
+    <UserPoints value={10000} isLoaded /> <br />
     Use knobs to change: <br />
-    <UserPoints value={number('points', 10)} />
+    <UserPoints
+      value={number('points', 10)}
+      isLoaded={boolean('is loading', false)}
+    />
   </>
 );
 

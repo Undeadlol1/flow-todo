73181b78d891338d5d582b43587c911a1e176b36
Mihail Paley <undeadlol1@mail.ru>
2020-10-23 23:45:33 +0500

    feat: add submit button and errors to UpsertDailyGoal

--- src/components/unsorted/UpsertDailyGoal.tsx
+++ src/components/unsorted/UpsertDailyGoal.tsx
@@ -1,8 +1,14 @@
-import { Box, TextField, Theme } from '@material-ui/core';
+import {
+  Box,
+  Button,
+  Grid,
+  Theme,
+} from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 import classNames from 'classnames';
-import { Formik } from 'formik';
+import { Field, Form, Formik } from 'formik';
 import React, { memo } from 'react';
+import { TextField } from 'formik-material-ui';
 
 const useStyles = makeStyles((theme: Theme) => ({ root: {} }));
 
@@ -20,7 +26,7 @@ const UpsertDailyGoal = memo((props: Props) => {
         initialValues={{ title: '' }}
         validate={values => {
           const errors: Partial<{ title: string }> = {};
-          if (!values.title) {
+          if (!values.title?.trim()) {
             errors.title = 'Required';
           }
           return errors;
@@ -33,17 +39,33 @@ const UpsertDailyGoal = memo((props: Props) => {
         }}
       >
         {({ values, handleChange, handleBlur, handleSubmit }) => (
-          <form onSubmit={handleSubmit}>
-            <TextField
-              fullWidth
-              name="title"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.title}
-              variant="outlined"
-              label={'Title'}
-            />
-          </form>
+          <Form onSubmit={handleSubmit}>
+            <Grid container>
+              <Grid item xs>
+                  <Field
+                  fullWidth
+                  name="title"
+                  // TODO: i18n
+                  label="Название"
+                  variant="outlined"
+                  onBlur={handleBlur}
+                  value={values.title}
+                  component={TextField}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={3}>
+                <Button
+                  type="submit"
+                  color="primary"
+                  variant="contained"
+                >
+                  {/*  TODO: i18n */}
+                  Сохранить
+                </Button>
+              </Grid>
+            </Grid>
+          </Form>
         )}
       </Formik>
     </Box>

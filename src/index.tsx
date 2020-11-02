7f8501a15db61b770302922ae9d5f6c38e23cac4
Mihail Paley <undeadlol1@mail.ru>
2020-11-02 12:18:39 +0500

    fix: :arrow_up: migrate to new service worker

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// Make app work offline and load faster.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-if (process.env.NODE_ENV === 'production') serviceWorker.register();
-else serviceWorker.unregister();

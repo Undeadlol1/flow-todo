a5437d4b053056f988fe16cbe936b9087367d726
undeadlol1 <undeadlol1@mail.ru>
2020-11-06 20:00:28 +0500

    fix: wrong import

--- src/repositories/claimReward.ts
+++ src/repositories/claimReward.ts
@@ -1,4 +1,4 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
 import { getFirestore, handleErrors } from '../services/index';
 import { Reward } from '../store/rewardsSlice';
 

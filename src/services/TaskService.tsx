eff89d629b94c224f27973510b584cace5b43230
undeadlol1 <undeadlol1@mail.ru>
2020-11-06 16:51:54 +0500

    fix: :bug: "isStale" not detecting stale tasks

--- src/services/TaskService.tsx
+++ src/services/TaskService.tsx
@@ -4,6 +4,11 @@ import find from 'lodash/find';
 import get from 'lodash/fp/get';
 import { Task } from '../entities/Task';
 import { getFirestore } from './index';
+import debug from 'debug';
+
+const log = debug('TaskService');
+// TODO: remove this line.
+debug.enable('TaskService');
 
 export default class TaskService {
   static get db() {
@@ -43,8 +48,18 @@ export default class TaskService {
       differenceInDays(task?.createdAt || today, today) > 3;
     const isTaskUpdatedLongAgo =
       differenceInDays(task?.updatedAt || today, today) > 3;
-    return (
-      (isTaskOld && isEmpty(task.history)) || isTaskUpdatedLongAgo
-    );
+    const result =
+      (isTaskOld && isEmpty(task.history)) ||
+      isTaskUpdatedLongAgo ||
+      // NOTE: At first i didn't have "createdAt" property.
+      // TODO: remove this line in the future.
+      task?.createdAt === undefined;
+
+    console.log('isStale is called. %O', task);
+    console.log('isTaskOld: ', isTaskOld);
+    console.log('isTaskUpdatedLongAgo: ', isTaskUpdatedLongAgo);
+    console.log('result: ', result);
+
+    return result;
   }
 }

1db77ae2ed3d4b4b4962d142d765fd75f08d9eab
Mihail Paley <undeadlol1@mail.ru>
2020-11-10 15:10:11 +0500

    feat: :construction: adding slider

--- src/components/unsorted/WhatDoYouFeelSlider.tsx
+++ src/components/unsorted/WhatDoYouFeelSlider.tsx
@@ -1,4 +1,4 @@
-import { Box, Fab, Grid, Theme } from '@material-ui/core';
+import { Box, Fab, Grid, Slider, Theme } from '@material-ui/core';
 import SatisfiedIcon from '@material-ui/icons/SentimentSatisfiedAlt';
 import DissatisfiedIcon from '@material-ui/icons/SentimentVeryDissatisfied';
 import { makeStyles } from '@material-ui/styles';
@@ -8,6 +8,33 @@ import { Link } from 'react-router-dom';
 const useStyles = makeStyles((theme: Theme) => ({ root: {} }));
 
 interface WhatDoYouFeelSliderProps {}
+
+const marks = [
+  {
+    value: 0,
+    label: '0°C',
+  },
+  {
+    value: 20,
+    label: '20°C',
+  },
+  {
+    value: 37,
+    label: '37°C',
+  },
+  {
+    value: 100,
+    label: '100°C',
+  },
+];
+
+function valuetext(value: number) {
+  return `${value}°C`;
+}
+
+function valueLabelFormat(value: number) {
+  return marks.findIndex((mark) => mark.value === value) + 1;
+}
 
 const WhatDoYouFeelSlider = memo(function WhatDoYouFeelSlider(
   props: WhatDoYouFeelSliderProps,
@@ -21,6 +48,17 @@ const WhatDoYouFeelSlider = memo(function WhatDoYouFeelSlider(
           <Fab component={Link} color="secondary" to={`$isGood`}>
             <SatisfiedIcon fontSize="large" />
           </Fab>
+        </Grid>
+        <Grid item xs>
+          <Slider
+            defaultValue={20}
+            valueLabelFormat={valueLabelFormat}
+            getAriaValueText={valuetext}
+            aria-labelledby="discrete-slider-restrict"
+            step={null}
+            valueLabelDisplay="auto"
+            marks={marks}
+          />
         </Grid>
         <Grid item xs>
           <Fab

723500db42573c57db8a799f19d8dea732f7a0fd
Mihail Paley <undeadlol1@mail.ru>
2020-11-20 23:44:09 +0500

    feat: change linear progress bar to circular in TasksDoneToday

--- src/components/tasks/TasksDoneToday.tsx
+++ src/components/tasks/TasksDoneToday.tsx
@@ -1,18 +1,18 @@
+import { CircularProgress, Theme, Zoom } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import MobileStepper from '@material-ui/core/MobileStepper';
 import Typography from '@material-ui/core/Typography';
+import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
 import Skeleton from '@material-ui/lab/Skeleton';
 import { makeStyles, withStyles } from '@material-ui/styles';
-import React from 'react';
-import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
-import { Theme, Zoom } from '@material-ui/core';
 import classNames from 'classnames';
-import { useTypedTranslate } from '../../services/index';
-import DayliTasksStreak from './DayliTasksStreak';
+import React from 'react';
 import { IDayliStreak } from '../../entities/IDayliStreak';
+import { useTypedTranslate } from '../../services/index';
 import { NumbersAnimatedOnUpdate } from '../ui/NumbersAnimatedOnUpdate';
+import DayliTasksStreak from './DayliTasksStreak';
 
 const useStyles = makeStyles((theme: Theme) => ({
   successIcon: {
@@ -58,49 +58,62 @@ function TasksDoneToday({
   if (!props.isLoaded)
     return <Skeleton variant="rect" height="160px" />;
   return (
-    <Card>
-      <CardContent>
-        <Box mb={2}>
-          <Typography variant="h6" display="inline">
-            {`${t('completed_tasks_today')}: `}
-            <span
-              className={classNames({
-                [classes.boldText]: completedTasksInpercentages > 100,
-              })}
-            >
-              <NumbersAnimatedOnUpdate
-                value={completedTasksInpercentages}
-                isAnimationDisabled={isUpdateAnimationDisabled}
-              />
-              {`${completedTasksInpercentages > 0 ? '%' : ''}`}
-            </span>
-          </Typography>
-          <Box display="inline" className={classes.successIcon}>
-            {isAchieved && (
-              <Zoom in>
-                <CheckCircleOutlineIcon />
-              </Zoom>
-            )}
+    <>
+      <Card>
+        <CardContent>
+          <Box
+            mb={2}
+            style={{ display: 'inline-block', float: 'left' }}
+          >
+            <Typography variant="h6" display="inline">
+              {`${t('completed_tasks_today')}: `}
+              <span
+                className={classNames({
+                  [classes.boldText]:
+                    completedTasksInpercentages > 100,
+                })}
+              >
+                <NumbersAnimatedOnUpdate
+                  value={completedTasksInpercentages}
+                  isAnimationDisabled={isUpdateAnimationDisabled}
+                />
+                {`${completedTasksInpercentages > 0 ? '%' : ''}`}
+              </span>
+            </Typography>
+            <Box display="inline" className={classes.successIcon}>
+              {isAchieved && (
+                <Zoom in>
+                  <CheckCircleOutlineIcon />
+                </Zoom>
+              )}
+            </Box>
           </Box>
-        </Box>
-        <StyledMobileStepper
-          position="static"
-          variant="progress"
-          nextButton={<div />}
-          backButton={<div />}
-          steps={tasksPerDay + 1}
-          activeStep={
-            tasksToday > tasksPerDay ? tasksPerDay : tasksToday
-          }
-        />
-        <Box mt={2}>
-          <DayliTasksStreak
-            streak={props.dailyStreak}
-            isUpdateAnimationDisabled={isUpdateAnimationDisabled}
-          />
-        </Box>
-      </CardContent>
-    </Card>
+          {/* <StyledMobileStepper
+            position="static"
+            variant="progress"
+            nextButton={<div />}
+            backButton={<div />}
+            steps={tasksPerDay + 1}
+            activeStep={
+              tasksToday > tasksPerDay ? tasksPerDay : tasksToday
+            }
+          /> */}
+          <Box style={{ display: 'inline-block', float: 'right' }}>
+            <CircularProgress
+              size={150}
+              variant="static"
+              value={completedTasksInpercentages}
+            />
+          </Box>
+          <Box mt={2} style={{ clear: 'both' }}>
+            <DayliTasksStreak
+              streak={props.dailyStreak}
+              isUpdateAnimationDisabled={isUpdateAnimationDisabled}
+            />
+          </Box>
+        </CardContent>
+      </Card>
+    </>
   );
 }
 

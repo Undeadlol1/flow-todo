8142b223079558d11192f47d28dc8227766eb711
undeadlol1 <undeadlol1@mail.ru>
2020-11-25 02:53:59 +0500

    feat: :construction:

--- src/components/tasks/DayliTasksStreak.tsx
+++ src/components/tasks/DayliTasksStreak.tsx
@@ -1,11 +1,14 @@
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
+import { subDays } from 'date-fns/esm';
 import debug from 'debug';
 import React, { memo } from 'react';
-import { useTypedTranslate } from '../../services';
-import DailyStreak from '../../services/dailyStreak';
 import { IDayliStreak } from '../../entities/IDayliStreak';
-import { NumbersAnimatedOnUpdate } from '../ui/NumbersAnimatedOnUpdate';
+import {
+  distanceBetweenDates,
+  useTypedTranslate,
+} from '../../services';
+import DailyStreak from '../../services/dailyStreak';
 
 const componentName = 'DayliTasksStreak';
 const log = debug(componentName);
@@ -20,6 +23,10 @@ const DayliTasksStreak = memo(
     const t = useTypedTranslate();
 
     let daysInARow = DailyStreak.daysInARow(streak);
+    let relative = distanceBetweenDates(
+      streak.updatedAt || Date.now(),
+      streak.startsAt || Date.now(),
+    );
     const daysSinceUpdate = DailyStreak.daysSinceUpdate(streak);
 
     log('streak.startsAt: ', new Date(streak?.startsAt as number));
@@ -28,16 +35,15 @@ const DayliTasksStreak = memo(
     log('daysSinceUpdate: ', daysSinceUpdate);
 
     if (daysSinceUpdate === undefined || daysSinceUpdate > 1) {
-      daysInARow = 0;
+      relative = distanceBetweenDates(
+        streak.startsAt || Date.now(),
+        subDays(streak.updatedAt || Date.now(), 1),
+      );
     }
     return (
       <Typography variant="h6">
         <Box fontWeight={100}>
-          {t('won_days_in_a_row')}:{' '}
-          <NumbersAnimatedOnUpdate
-            value={daysInARow}
-            isAnimationDisabled={isUpdateAnimationDisabled}
-          />
+          {t('won_days_in_a_row')}: {relative}
         </Box>
       </Typography>
     );

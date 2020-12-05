fa0edb991168473af1bbfa791c59a7c89ba07c66
undeadlol1 <undeadlol1@mail.ru>
2020-12-05 08:17:09 +0500

    refactor: :recycle: ExperienceProgressBar

--- src/components/users/ExpirienceProgressBar/ExpirienceProgressBar.tsx
+++ src/components/users/ExpirienceProgressBar/ExpirienceProgressBar.tsx
@@ -1,17 +1,20 @@
+import { Box, Theme } from '@material-ui/core';
 import LinearProgress from '@material-ui/core/LinearProgress';
-import { makeStyles } from '@material-ui/styles';
 import Tooltip from '@material-ui/core/Tooltip';
+import { makeStyles } from '@material-ui/styles';
 import cx from 'clsx';
 import debug from 'debug';
 import get from 'lodash/get';
 import isUndefined from 'lodash/isUndefined';
 import React, { memo } from 'react';
-import { Box, Theme } from '@material-ui/core';
-import LevelingService from '../../../services/Leveling';
 import { Profile } from '../../../entities/Profile';
+import LevelingService from '../../../services/Leveling';
 
 const log = debug('ExpirienceProgressBar');
 const useStyles = makeStyles((theme: Theme) => ({
+  hidden: {
+    opacity: 0,
+  },
   progress: {
     left: 0,
     bottom: 0,
@@ -19,16 +22,19 @@ const useStyles = makeStyles((theme: Theme) => ({
     width: '100%',
     position: 'fixed',
   },
-  hidden: {
-    opacity: 0,
+  animatedNumbers: {
+    bottom: 0,
+    height: 10,
+    left: '50%',
+    width: '100%',
+    position: 'fixed',
   },
 }));
 
 export const ExpirienceProgressBar: React.FC<{
   profile?: Profile;
-  className?: string;
   isAnimationActive: boolean;
-}> = memo((props) => {
+}> = memo(({ isAnimationActive, ...props }) => {
   const classes = useStyles();
   const userPoints = get(props.profile, 'experience', 0);
   const level = LevelingService.calculateUserLevel(userPoints);
@@ -48,18 +54,18 @@ export const ExpirienceProgressBar: React.FC<{
 
   log('level', level);
   log('userPoints', userPoints);
+  log('progressPercent: ', progressPercent);
   log(
     'points to next level',
     pointsToReachPreviousLevel +
       LevelingService.calculatePointsToNextLevel(level),
   );
-  log('progressPercent: ', progressPercent);
 
   if (isUndefined(props.profile)) {
     return (
       <LinearProgress
         color="secondary"
-        className={cx([classes.progress, props.className])}
+        className={classes.progress}
       />
     );
   }
@@ -74,19 +80,17 @@ export const ExpirienceProgressBar: React.FC<{
         <LinearProgress
           color="secondary"
           value={progressPercent === 100 ? 0 : progressPercent}
-          className={cx(!props.profile && classes.hidden, [
+          className={cx(
             classes.progress,
-            props.className,
-          ])}
+            !props.profile && classes.hidden,
+          )}
           variant={
-            props.isAnimationActive ? 'indeterminate' : 'determinate'
+            isAnimationActive ? 'indeterminate' : 'determinate'
           }
         />
       </Tooltip>
     </Box>
   );
 });
-
-ExpirienceProgressBar.displayName = 'ExpirienceProgressBar';
 
 export default ExpirienceProgressBar;

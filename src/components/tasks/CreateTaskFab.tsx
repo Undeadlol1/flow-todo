8a13a921d5b01d1398f5f3da1c4a39d25fb3c104
undeadlol1 <undeadlol1@mail.ru>
2020-12-12 17:04:19 +0500

    refactor: :fire: dead code

--- src/components/tasks/CreateTaskFab.tsx
+++ src/components/tasks/CreateTaskFab.tsx
@@ -1,27 +1,19 @@
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import AddIcon from '@material-ui/icons/Add';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import useToggle from 'react-use/lib/useToggle';
-import Dialog from '@material-ui/core/Dialog';
-import DialogContent from '@material-ui/core/DialogContent';
-import isEmpty from 'lodash/isEmpty';
-import AddIcon from '@material-ui/icons/Add';
-import cx from 'clsx';
 import Fab from '../ui/Fab';
 import UpsertTask from './CreateTask/UpsertTask';
 
 interface Props {
-  activeTasks?: [];
   isHidden?: boolean;
-  createdAtleastOneTask?: [];
 }
 
 export const CreateTaskFab: React.FC<Props> = (props) => {
   const [t] = useTranslation();
   const [isDialogOpen, toggleDialog] = useToggle(false);
-  const createdTasksBefore = !isEmpty(props.createdAtleastOneTask);
-  const fabClasses = cx(
-    !createdTasksBefore && 'animated pulse infinite',
-  );
   return (
     <>
       <Dialog
@@ -38,16 +30,11 @@ export const CreateTaskFab: React.FC<Props> = (props) => {
         </DialogContent>
       </Dialog>
       <Fab
-        className={fabClasses}
         isHidden={props.isHidden}
         aria-label={t('createTask')}
         onClick={toggleDialog}
       >
-        {createdTasksBefore && isEmpty(props.activeTasks) ? (
-          '+10'
-        ) : (
-          <AddIcon />
-        )}
+        <AddIcon />
       </Fab>
     </>
   );
